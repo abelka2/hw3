d0f6cff72ce7b9a7a23dec9e57be5a77f63d8385
Admin <admin@dhcp-vlan876-9-216-214.vlan861.client.wireless.uic.edu>
2018-10-17 17:24:30 -0500

    check 2

--- hw3.c
+++ hw3.c
@@ -75,10 +75,10 @@ void forkChild(char *args[], int n){
 
   }
   else{
-    wait(&status);
+    //wait(&status);
     printf("pid:%d", getpid());
     printf(" status:%d\n", WEXITSTATUS(status));
-    //wait(&status);
+    wait(&status);
     //printf("Exit: %d\n", status);
   }
 }
